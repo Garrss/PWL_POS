40cfe66e3475e1af4be2a56840e9491c409234c7
Muhammad Tegar Hibatulloh <07mtegar@gmail.com>
2025-03-11 00:44:30 +0700

    Practicum week 4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,19 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $table = 'm_user';  // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
     /**
-     * The attribute that awe mass assignable.
+     * The attributes that are mass assignable
      * 
-     * @var array
+     *  @var array
      */
+    protected $fillable = ['level_id', 'username', 'name', 'password'];
 
-    protected $fillable = ['level_id', 'username', 'name'];
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,24 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Data User</title>
 </head>
 <body>
     <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0" >
+    <a href="/user/tambah">+ Tambah User</a>
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
-            <th>Name</th>
+            <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <th>Kode Level</th>
+            <th>Nama Level</th>
+            <th>Aksi</th>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->name }}</td>
-            <td>{{ $data->level_id }}</td>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->name }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td>{{ $d->level->level_kode }}</td>
+            <td>{{ $d->level->level_nama }}</td>
+            <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
         </tr>
-
+        @endforeach
     </table>
 </body>
 </html>
